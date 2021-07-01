0fed7621c04debad64ea6455455494d4e6eb03fa
Diego Zamboni <diego@zzamboni.org>
2021-07-01 21:50:35 +0200

    Addressed MELPA maintainer comments, added dependency to ox-750words.el

--- ox-750words.el
+++ ox-750words.el
@@ -1,7 +1,7 @@
 ;;; ox-750words.el --- Org mode exporter for 750words.com -*- lexical-binding: t; -*-
-;;
+
 ;; Copyright (C) 2021 Diego Zamboni
-;;
+
 ;; Author: Diego Zamboni <https://github.com/zzamboni>
 ;; Maintainer: Diego Zamboni <diego@zzamboni.org>
 ;; Created: June 10, 2021
@@ -9,28 +9,28 @@
 ;; Version: 0.0.1
 ;; Keywords: files, org, writing
 ;; Homepage: https://github.com/zzamboni/750words-client
-;; Package-Requires: ((emacs "24.4"))
-;;
+;; Package-Requires: ((emacs "24.4") (750words "0.0.1"))
+
 ;; This file is not part of GNU Emacs.
-;;
+
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
 ;; You may obtain a copy of the License at
-;;
+
 ;;     https://www.apache.org/licenses/LICENSE-2.0
-;;
+
 ;; Unless required by applicable law or agreed to in writing, software
 ;; distributed under the License is distributed on an "AS IS" BASIS,
 ;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
-;;
+
 ;;; Commentary:
-;;
+
 ;; An Org exporter which converts Org to Markdown and posts it to 750words.com.
-;;
+
 ;; See https://github.com/zzamboni/750words-client for full usage instructions.
-;;
+
 ;;; Code:
 
 (require '750words)
